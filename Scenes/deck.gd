519388d8b47b03c9fe02dd147fccf6798aee9aa1
Aryo Nazaradeh <aryo.nazaradeh@gmail.com>
2024-11-15 14:16:55 -0800

    Play around with deck hand container

--- Scenes/deck.gd
+++ Scenes/deck.gd
@@ -3,4 +3,5 @@ extends Node
 func _on_end_turn_pressed() -> void:
 	var scene = load("res://Scenes/vigilant_of_stendarr.tscn")
 	var card = scene.instantiate()
-	add_child(card)
+	var player_hand = get_parent().get_node("PlayerHand")
+	player_hand.add_child(card)
